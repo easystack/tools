3bbf24f42f287d7a09d2c5daeac3f2c2801405b2
xiaoqiang.liu <xiaoqiang.liu@easystack.cn>
2023-12-11 18:08:30 +0800

    set ansible hosts

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "[all]" > hosts
-ssh node-1 kubectl get node -l node-role.kubernetes.io/node=true|grep node-|awk '{print $1}'>>hosts
-export ANSIBLE_HOST_KEY_CHECKING=False
+# 在eks部署机执行
 
+echo "[all]" > hosts
+# ssh node-1 kubectl get node -l node-role.kubernetes.io/node=true|grep node-|awk '{print $1}'>>hosts
+ssh node-1 kubectl get node |grep node-|awk '{print $1}'>>hosts
+
+export ANSIBLE_HOST_KEY_CHECKING=False
 ansible all -i hosts -m script -a "config_registry.sh"
--- es-test.sh
+++ es-test.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+export BUILD_WITH_CONTAINER=0
 make stability
 sleep 120
 ./metrics/check_metrics.py
+
+# TODO 

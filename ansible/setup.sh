8b0df136939ad858846095e7cdcc3a1e1f4e2016
xiaoqiang.liu <xiaoqiang.liu@easystack.cn>
2023-12-11 16:45:40 +0800

    set ansible node

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "[all]" > hosts
-cat /etc/hosts|grep node-|awk '{print $2}' >> hosts
+ssh node-1 kubectl get node -l node-role.kubernetes.io/node=true|grep node-|awk '{print $1}'>>hosts
 export ANSIBLE_HOST_KEY_CHECKING=False
 
 ansible all -i hosts -m script -a "config_registry.sh"
